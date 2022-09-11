a4ccf88b74446b396bc880c3b995acf608a2b842
Rewat Boonsit <ravatna@gmail.com>
2022-09-11 18:41:32 +0700

    update opensea script

---
+++ setup.bat
@@ -0,0 +1,14 @@
+
+npm install --global yarn
+
+yarn --version
+
+yarn init -y
+
+yarn add -D typescript ts-node @types/node
+
+npm install
+
+yarn tsc --init
+
+yarn add csv-parse
---
+++ setup.sh
@@ -0,0 +1,12 @@
+
+npm install --global yarn
+
+yarn --version
+
+yarn init -y
+
+yarn add -D typescript ts-node @types/node
+
+yarn tsc --init
+
+yarn add csv-parse
